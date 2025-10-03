5a8b34c22fa2d79d05eecff5dc3c82525556944a
akos <szmoleniczki.akos@edu.bme.hu>
2025-10-03 19:11:23 +0200

    added caching

--- src/main.py
+++ src/main.py
@@ -11,6 +11,9 @@ from gensim.models import FastText
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import hashlib
+import os
+import pickle
 
 # Download nltk stuff if not downloaded already
 try:
@@ -21,6 +24,21 @@ try:
     nltk.download('stopwords', quiet=True)
 except:
     pass
+
+def file_hash(file):
+    with open(file, 'rb') as f:
+        return hashlib.md5(f.read()).hexdigest()
+
+def cache_exists(cache_name, ext='.pkl'):
+    return os.path.exists(os.path.join(CACHE_DIR, f"{cache_name}{ext}"))
+
+def save_cache(data, cache_name):
+    with open(os.path.join(CACHE_DIR, f"{cache_name}.pkl"), 'wb') as f:
+        pickle.dump(data, f)
+
+def load_cache(cache_name):
+    with open(os.path.join(CACHE_DIR, f"{cache_name}.pkl"), 'rb') as f:
+        return pickle.load(f)
 
 # Converts treebank tag to wordnet pos tag
 def get_wordnet_pos(treebank_tag):
@@ -58,19 +76,52 @@ def document_vector(tokens, model):
     else:
         return np.zeros(model.vector_size)
 
-dataframe = pd.read_csv('data/dataset-tickets-multi-lang-5-2-50-version.csv')
+CACHE_DIR = 'cache'
+if not os.path.isdir(CACHE_DIR):
+    os.mkdir(CACHE_DIR)
+
+data_path = 'data/dataset-tickets-multi-lang-5-2-50-version.csv'
+data_hash = file_hash(data_path)
+
+dataframe = pd.read_csv(data_path)
 dataframe_en = dataframe[dataframe['language'] == 'en'].copy()
-clean_docs = [clean_document(body) for body in dataframe_en['body']]
+
+# Clean documents
+if cache_exists(f'clean_docs_{data_hash}'):
+    print('Loading clean documents from cache...')
+    clean_docs = load_cache(f'clean_docs_{data_hash}')
+else:
+    print('Cleaning documents...')
+    clean_docs = [clean_document(body) for body in dataframe_en['body']]
+    save_cache(clean_docs, f'clean_docs_{data_hash}')
+    print(f'Clean documents cached')
 
 print(f'Number of documents: {len(clean_docs)}')
-print(f'Sample:\n{clean_docs[0]}')
+print(f'Sample original:\n{dataframe_en["body"][1]}')
+print(f'Sample clean:\n{clean_docs[0]}')
 
-ft_model = FastText(vector_size=100, window=5, min_count=1, workers=4, sg=1)
-ft_model.build_vocab(clean_docs)
-ft_model.train(clean_docs, total_examples=len(clean_docs), epochs=100)
+# Train model
+if cache_exists(f'fasttext_model_{data_hash}', '.model'):
+    print('Loading trained model from cache...')
+    ft_model = FastText.load(os.path.join(CACHE_DIR, f'fasttext_model_{data_hash}.model'))
+else:
+    print('Training FastText model...')
+    ft_model = FastText(vector_size=100, window=5, min_count=1, workers=4, sg=1)
+    ft_model.build_vocab(clean_docs)
+    ft_model.train(clean_docs, total_examples=len(clean_docs), epochs=100)
+    ft_model.save(os.path.join(CACHE_DIR, f'fasttext_model_{data_hash}.model'))
+    print('FastText model cached')
 
-# X
-doc_vectors = [document_vector(doc, ft_model) for doc in clean_docs]
+# Document vectors (= X)
+if cache_exists(f'doc_vectors_{data_hash}'):
+    print('Loading document vectors from cache...')
+    doc_vectors = load_cache(f'doc_vectors_{data_hash}')
+else:
+    print('Calculating document vectors...')
+    doc_vectors = [document_vector(doc, ft_model) for doc in clean_docs]
+    save_cache(doc_vectors, f'doc_vectors_{data_hash}')
+    print('Document vectors cached')
+
 print(f'Sample document vector:\n{doc_vectors[0]}')
 
 # y
