069e9aba04565e376bcb12556a7c51fefaed0924
akos <szmoleniczki.akos@edu.bme.hu>
2025-10-27 13:28:27 +0100

    modularized main

--- src/main.py
+++ src/main.py
@@ -1,97 +1,28 @@
 import pandas as pd
-import nltk
-from nltk.corpus import wordnet
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
-from nltk import pos_tag
-from nltk import word_tokenize
-import string
-import re
 from gensim.models import FastText
-import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import hashlib
-import os
-import pickle
 
-# Download nltk stuff if not downloaded already
-try:
-    nltk.download('wordnet', quiet=True)
-    nltk.download('averaged_perceptron_tagger_eng', quiet=True)
-    nltk.download('omw-1.4', quiet=True)
-    nltk.download('punkt_tab', quiet=True)
-    nltk.download('stopwords', quiet=True)
-except:
-    pass
-
-def file_hash(file):
-    with open(file, 'rb') as f:
-        return hashlib.md5(f.read()).hexdigest()
-
-def cache_exists(cache_name, ext='.pkl'):
-    return os.path.exists(os.path.join(CACHE_DIR, f"{cache_name}{ext}"))
-
-def save_cache(data, cache_name):
-    with open(os.path.join(CACHE_DIR, f"{cache_name}.pkl"), 'wb') as f:
-        pickle.dump(data, f)
-
-def load_cache(cache_name):
-    with open(os.path.join(CACHE_DIR, f"{cache_name}.pkl"), 'rb') as f:
-        return pickle.load(f)
-
-# Converts treebank tag to wordnet pos tag
-def get_wordnet_pos(treebank_tag):
-    if treebank_tag.startswith('J'):
-        return wordnet.ADJ
-    elif treebank_tag.startswith('V'):
-        return wordnet.VERB
-    elif treebank_tag.startswith('N'):
-        return wordnet.NOUN
-    elif treebank_tag.startswith('R'):
-        return wordnet.ADV
-    else:
-        return wordnet.NOUN
-
-# Tokenizes and lemmatizes the given text
-def clean_document(text):
-    lemmatizer = WordNetLemmatizer()
-    text = re.sub(r'\\[nrtbv]+', ' ', text)
-    tokens = [token.lower().strip() for token in word_tokenize(text) if (token not in set(stopwords.words('english')) and token not in list(string.punctuation))]
-    tagged_tokens = pos_tag(tokens)
-    lemmas = [lemmatizer.lemmatize(tagged_token[0], get_wordnet_pos(tagged_token[1])) for tagged_token in tagged_tokens]
-    return lemmas
-
-# Get vector for a document given by its tokens
-def document_vector(tokens, model):
-    word_vectors = []
-    for token in tokens:
-        if token in model.wv:
-            word_vectors.append(model.wv[token])
-    
-    if word_vectors:
-        return np.mean(word_vectors, axis=0)
-    else:
-        return np.zeros(model.vector_size)
-
-CACHE_DIR = 'cache'
-if not os.path.isdir(CACHE_DIR):
-    os.mkdir(CACHE_DIR)
+import preprocess
+import cache
+import vector
 
 data_path = 'data/dataset-tickets-multi-lang-5-2-50-version.csv'
-data_hash = file_hash(data_path)
-
 dataframe = pd.read_csv(data_path)
 dataframe_en = dataframe[dataframe['language'] == 'en'].copy()
+data_hash = cache.hash(data_path)
 
 # Clean documents
-if cache_exists(f'clean_docs_{data_hash}'):
+clean_documents_path = f'cache/clean_documents_{data_hash}'
+if cache.exists(clean_documents_path):
     print('Loading clean documents from cache...')
-    clean_docs = load_cache(f'clean_docs_{data_hash}')
+    clean_docs = cache.load(clean_documents_path)
 else:
     print('Cleaning documents...')
-    clean_docs = [clean_document(body) for body in dataframe_en['body']]
-    save_cache(clean_docs, f'clean_docs_{data_hash}')
+    tokenized_documents = [preprocess.tokenize(body) for body in dataframe_en['body']]
+    tokenized_documents = [preprocess.remove_stopwords(tokens) for tokens in tokenized_documents]
+    clean_docs = [preprocess.lemmatize(tokens) for tokens in tokenized_documents]
+    cache.save(clean_documents_path, clean_docs)
     print(f'Clean documents cached')
 
 print(f'Number of documents: {len(clean_docs)}')
@@ -99,25 +30,27 @@ print(f'Sample original:\n{dataframe_en["body"][1]}')
 print(f'Sample clean:\n{clean_docs[0]}')
 
 # Train model
-if cache_exists(f'fasttext_model_{data_hash}', '.model'):
+model_path = f'cache/model_{data_hash}'
+if cache.exists(model_path):
     print('Loading trained model from cache...')
-    ft_model = FastText.load(os.path.join(CACHE_DIR, f'fasttext_model_{data_hash}.model'))
+    ft_model = FastText.load(model_path)
 else:
     print('Training FastText model...')
     ft_model = FastText(vector_size=150, window=7, min_count=2, workers=4, sg=1, epochs=50, sample=1e-5)
     ft_model.build_vocab(clean_docs)
     ft_model.train(clean_docs, total_examples=len(clean_docs), epochs=50)
-    ft_model.save(os.path.join(CACHE_DIR, f'fasttext_model_{data_hash}.model'))
+    ft_model.save(model_path)
     print('FastText model cached')
 
 # Document vectors (= X)
-if cache_exists(f'doc_vectors_{data_hash}'):
+vectors_path = f'cache/vectors_{data_hash}'
+if cache.exists(vectors_path):
     print('Loading document vectors from cache...')
-    doc_vectors = load_cache(f'doc_vectors_{data_hash}')
+    doc_vectors = load_cache(vectors_path)
 else:
     print('Calculating document vectors...')
-    doc_vectors = [document_vector(doc, ft_model) for doc in clean_docs]
-    save_cache(doc_vectors, f'doc_vectors_{data_hash}')
+    doc_vectors = [vector.of(doc, ft_model) for doc in clean_docs]
+    cache.save(vectors_path, doc_vectors)
     print('Document vectors cached')
 
 print(f'Sample document vector:\n{doc_vectors[0]}')
